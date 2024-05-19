bca4365b8bd72fd6a18543ff2c75750a887ce5be
刘献强 <liuxq@hanwei.com>
2024-05-19 20:52:10 +0800

    修改标题

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,9 +13,9 @@ export default defineUserConfig({
   //网站语言，默认为中文
   lang: "zh-CN",
   //网站标题
-  title: "LearnData-开源笔记",
+  title: "我的世界",
   //网站描述
-  description: "开源工具、效率方法、心理学探索的自我提升笔记，记录并输出一切能让自己提升的知识。",
+  description: "永远相信，美好的日子即将来到。",
 
   //网站路径，默认为 /，如果非主域名，需改为
   base: "/",
